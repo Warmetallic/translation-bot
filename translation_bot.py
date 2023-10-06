0d2b6196de6c8572611f139f8935061865064d12
Gleb Kharlamov <scream10@list.ru>
2023-10-06 18:28:58 +0300

    added inlineButtons and callbacks

--- translation_bot.py
+++ translation_bot.py
@@ -1,5 +1,6 @@
 from decouple import config
 import telebot
+from telebot import types
 import requests
 import json
 
@@ -16,7 +17,15 @@ deepl_api_endpoint = "https://api-free.deepl.com/v2/translate"
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, "test")
+    markup = types.InlineKeyboardMarkup()
+    bt1 = types.InlineKeyboardButton("РУ 🇷🇺", callback_data="RUS")
+    bt2 = types.InlineKeyboardButton("ENG 🇬🇧", callback_data="ENG")
+    markup.add(bt1, bt2, row_width=2)
+    bot.send_message(
+        message.chat.id,
+        "Выберите на какой язык хотите перевести текст:\nChoose which language you need to translate into:",
+        reply_markup=markup,
+    )
 
 
 @bot.message_handler(content_types=["text"])
@@ -39,6 +48,16 @@ def get_text(message):
         bot.reply_to(message, translated_text)
     else:
         bot.reply_to(message, "Translation failed. Please try again later.")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if "RUS" in call.data:
+        bot.send_message(call.message.chat.id, "Введите текст для перевода на русский:")
+    elif "ENG" in call.data:
+        bot.send_message(
+            call.message.chat.id, "Введите текст для перевода на английский:"
+        )
 
 
 bot.infinity_polling()
