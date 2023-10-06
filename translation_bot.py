957e1150eae310c4cc81fce700c7de1ccd3c995f
Gleb Kharlamov <scream10@list.ru>
2023-10-06 21:28:22 +0300

    implemented db with create update history and delete functions

--- translation_bot.py
+++ translation_bot.py
@@ -3,6 +3,12 @@ import telebot
 import json
 from keyboards.inline_markup import create_language_markup
 from API.api_function import api_connect
+from database.db_functions import (
+    create_db,
+    update_user,
+    get_user_history,
+    delete_user_history,
+)
 
 
 # Getting secret keys from decouple
@@ -14,7 +20,11 @@ bot = telebot.TeleBot(bot_key)
 # DeepL API endpoint
 deepl_api_endpoint = "https://api-free.deepl.com/v2/translate"
 
+# Create SQLite database
+create_db()
 
+
+# Main function to open menu for translation
 @bot.message_handler(commands=["translate"])
 def start(message):
     markup = create_language_markup()
@@ -25,28 +35,41 @@ def start(message):
     )
 
 
-# @bot.message_handler(content_types=["photo"])
-# def get_text(message):
-#     input_text = message.text.strip().lower()
+# Handle the /history command
+@bot.message_handler(commands=["history"])
+def show_history(message):
+    user_id = message.from_user.id
+    history = get_user_history(user_id)
 
-#     # Specify the parameters for the DeepL API request
-#     params = {
-#         "auth_key": api_key,  # Your DeepL API key
-#         "text": input_text,
-#         "target_lang": "ru",  # Target language (e.g., Russian)
-#     }
+    if history:
+        response = "Translation History:\n\n"
+        for idx, entry in enumerate(history, 1):
+            original_text = entry["original_text"]
+            translated_text = entry["translated_text"]
+            response += (
+                f"{idx}. Original Text: {original_text}\nResult: {translated_text}\n\n"
+            )
+    else:
+        response = "Translation History is empty."
 
-#     # Make a GET request to the DeepL API
-#     res = requests.get(deepl_api_endpoint, params=params)
-
-#     if res.status_code == 200:
-#         data = json.loads(res.text)
-#         translated_text = data["translations"][0]["text"]
-#         bot.reply_to(message, translated_text)
-#     else:
-#         bot.reply_to(message, "Translation failed. Please try again later.")
+    bot.send_message(message.chat.id, response)
 
 
+# Handle the /delete_history command
+@bot.message_handler(commands=["delete_history"])
+def delete_history(message):
+    user_id = message.from_user.id
+    deleted_count = delete_user_history(user_id)
+
+    if deleted_count > 0:
+        response = f"Deleted {deleted_count} records from your Translation History."
+    else:
+        response = "No records found to delete in your Translation History."
+
+    bot.send_message(message.chat.id, response)
+
+
+# Handle callbacks from buttons
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
     if "RUS" in call.data:
@@ -64,6 +87,7 @@ def callback(call):
         )
 
 
+# Translate function, connects to API and return translated text
 def translate_function(message, target_lang):
     user_input = message.text
     # request to Deepl's API
@@ -77,6 +101,7 @@ def translate_function(message, target_lang):
             f"Translation: {translated_text}",
             reply_markup=create_language_markup(),
         )
+        update_user(message.from_user.id, user_input, translated_text)
 
     else:
         bot.reply_to(
